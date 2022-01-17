85659f3d1e002bdbf51a54868f935b2e8708324b
EugeneKhomitskiy <mtl_87@list.ru>
2022-01-17 13:12:51 +0300

    changes

--- app/src/main/java/com/example/markersyandexmap/application/MarkersApplication.kt
+++ app/src/main/java/com/example/markersyandexmap/application/MarkersApplication.kt
@@ -1,11 +1,12 @@
 package com.example.markersyandexmap.application
 
 import android.app.Application
+import com.example.markersyandexmap.BuildConfig
 import com.yandex.mapkit.MapKitFactory
 
 class MarkersApplication: Application() {
     override fun onCreate() {
         super.onCreate()
-        MapKitFactory.setApiKey("Api key")
+        MapKitFactory.setApiKey(BuildConfig.MAPS_API_KEY)
     }
 }
